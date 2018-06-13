fbedfbc4b380b35d650a40058d05f6311665f0fc
Vijay Thapa <vijaythapa333@gmail.com>
2018-06-13 15:16:14 +0545

    phpblog project Started for ASMT college student

---
+++ admin/users.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Panel for Our Blog</title>
+        
+        <link rel="stylesheet" type="text/css" href="http://localhost:81/phpblog/assets/css/style.css" />
+    </head>
+    
+    <body>
+    
+    <!-- Menu Starts From Here -->
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="users.php">Users</a>
+                </li>
+                <li>
+                    <a href="categories.php">Categories</a>
+                </li>
+                <li>
+                    <a href="blogs.php">Blogs</a>
+                </li>
+            </ul>
+        </nav>
+    <!-- Menu Ends From Here -->
+        
+    <!-- Main Content Starts Here -->
+    <section class="main">
+        <h1>User Page</h1>
+        
+        <p>
+            You'll Manage Backend from this section.
+        </p>
+    </section>
+    <!-- Main Content Starts Here -->
+        
+    <!-- Footer Starts Here -->
+    <footer>
+        &copy; 2018, PHP BLOG.
+    </footer>
+    <!-- Footer Starts Here -->
+    
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php 
+    echo "This is Front Page of Our Blog";
+?>
